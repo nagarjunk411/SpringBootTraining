36c9c96bf40ffdd638d12dd6b26fb44214ead1a6
KN00568010 <KN00568010@INHYIZPC12260.TechMahindra.com>
2019-08-06 17:17:13 +0530

    Initial Commit

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -0,0 +1,37 @@
+package com.appsdeveloperblog.photoapp.api.users.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	private Environment environment;
+	private UsersService usersService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		.and()
+		.addFilter(getAuthenticationFilter());
+		http.headers().frameOptions().disable();
+	}
+	
+	private AuthenticationFilter getAuthenticationFilter() throws Exception
+	{
+	
+
+}
